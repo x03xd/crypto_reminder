7c9d44e2cef267f458e7035ff72f5fa4bcc70544
x03 <darkpower289@gmail.com>
2023-09-19 12:44:37 +0200

    commit

--- src/Strategy.js
+++ src/Strategy.js
@@ -17,7 +17,6 @@ class StrategySort {
 export class SortingByAlpha extends StrategySort {
     
     sort(sort_type) {
-
         const imagesArray = Array.from(this.imageContainer.getElementsByTagName("img"));
 
         imagesArray.sort((a, b) => {
@@ -27,19 +26,49 @@ export class SortingByAlpha extends StrategySort {
             return sort_type === "standard" ? classA.localeCompare(classB) : classB.localeCompare(classA);
         });
 
+        if(imagesArray.length !== 0) this.add(imagesArray)
+    }
+
+
+    add(imagesArray) {
+        this.imageContainer.innerHTML = "";
+
+        for (const image of imagesArray) {
+            this.imageContainer.appendChild(image);
+        }
+    }
+
+}
+
+
+
+
+export class SortingByAlpha extends StrategySort {
+    
+    sort(sort_type) {
+        const imagesArray = Array.from(this.imageContainer.getElementsByTagName("img"));
+
+        imagesArray.sort((a, b) => {
+            const classA = a.className;
+            const classB = b.className;
+
+            return sort_type === "standard" ? classA.localeCompare(classB) : classB.localeCompare(classA);
+        });
 
         if(imagesArray.length !== 0) this.add(imagesArray)
     }
 
 
     add(imagesArray) {
-
         this.imageContainer.innerHTML = "";
 
         for (const image of imagesArray) {
             this.imageContainer.appendChild(image);
         }
-
     }
 
 }
+
+
+
+
